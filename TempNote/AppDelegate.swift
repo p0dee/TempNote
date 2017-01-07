2cd0a5779b0db1683ee6bfe5c24667d8aa5f3d24
p0dee <t.takeshi.uc@gmail.com>
2017-01-07 18:23:11 +0900

    プロジェクトの作成

---
+++ TempNote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TempNote
+//
+//  Created by Takeshi Tanaka on 2017/01/07.
+//  Copyright © 2017 p0dee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TempNote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TempNote
+//
+//  Created by Takeshi Tanaka on 2017/01/07.
+//  Copyright © 2017 p0dee. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
