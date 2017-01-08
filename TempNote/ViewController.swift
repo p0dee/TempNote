2cf2aee48cfda59e7ac7f8a1afa6e303765d0fd3
p0dee <t.takeshi.uc@gmail.com>
2017-01-08 22:13:58 +0900

    NSTextViewをwindow全体を覆うように配置してみた

--- TempNote/ViewController.swift
+++ TempNote/ViewController.swift
@@ -9,11 +9,13 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    lazy var textView = NSTextView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpViews()
+        setUpConstraints()
     }
 
     override var representedObject: Any? {
@@ -21,7 +23,27 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    override func viewDidAppear() {
+        super.viewDidAppear()
+    }
+    
+    //MARK: Set Up
+    private func setUpViews() {
+        view.addSubview(textView)
+        textView.insertText("Initial text", replacementRange: NSMakeRange(0, 0))
+    }
 
+    private func setUpConstraints() {
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        var cstrs = [NSLayoutConstraint]()
+        cstrs.append(NSLayoutConstraint(item: textView, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 0.0))
+        cstrs.append(NSLayoutConstraint(item: textView, attribute: .top, relatedBy: .equal, toItem: view, attribute: .top, multiplier: 1.0, constant: 0.0))
+        cstrs.append(NSLayoutConstraint(item: textView, attribute: .right, relatedBy: .equal, toItem: view, attribute: .right, multiplier: 1.0, constant: 0.0))
+        cstrs.append(NSLayoutConstraint(item: textView, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0.0))
+        NSLayoutConstraint.activate(cstrs)
+        view.layoutSubtreeIfNeeded()
+    }
 
 }
 
